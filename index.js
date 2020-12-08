dc3a1f31620fe32eca20559edcb5181b0a5f8926
Rakesh <rakesh1q2w@gmail.com>
2020-12-08 14:02:47 +0000

    sentence translated

--- index.js
+++ index.js
@@ -1,3 +1,82 @@
+// const express = require('express');
+// const lib = require("./lib");
+
+// var cors = require('cors')
+// var app = express()
+
+// app.use(cors())
+
+// app.get("/", (req, res) => {
+//   res.send(" Go to translate")
+// })
+
+
+
+// app.get('/translate/minion.json', (req, res) => {
+  
+//   var quer = req.query.text;
+//   var splitArray = quer.split(" ");
+//   // console.log(split.join(" "));
+
+
+//   for (j=0;j<splitArray.length;j++){
+//     var singleString = splitArray[j];
+     
+
+//     for (i=0;i<lib.length;i++){
+//       var libString = lib[i];
+//       var resArray = [];
+      
+//       if (libString.w.toUpperCase()==singleString.toUpperCase()){
+//         resArray.push(libString.r);
+//         var resString = resArray.join(' ');
+//         console.log(resArray);
+//       }else {
+//         resArray.push(singleString);
+//       }
+      
+//     }
+//   }
+
+
+
+
+
+
+
+  
+//   // for (i=0;i<lib.length;i++){
+//   //   var first=lib[i];     
+//   //   var second = [];
+
+//   //   for (j=0;j<split.length;j++){
+//   //     var querStr = split[j];
+//   //      if (first.w.toUpperCase() == querStr.toUpperCase()){
+//   //        second.push(first.r);
+//   //        var third = second.join(" ");
+//   //        console.log(third);
+//   //      }
+        
+//   //   }
+   
+
+//   // }
+//   res.json({
+//     "success": {
+//         "total": 1
+//     },
+//     "contents": {
+//         "translated": resString ,
+//         "text": req.query.text || "text missing",
+//         "translation": "minion"
+//     }
+// })
+// });
+
+// app.listen(3000, () => {
+//   console.log('server started');
+// });
+
 const express = require('express');
 const lib = require("./lib");
 
@@ -7,45 +86,51 @@ var app = express()
 app.use(cors())
 
 app.get("/", (req, res) => {
-  res.send(" Go to translate")
+    res.send(" Go to translate")
 })
 
 
 
 app.get('/translate/minion.json', (req, res) => {
-  
-  var quer = req.query.text;
-  var split = quer.split(" ");
-  // console.log(split.join(" "));
-  
-  for (i=0;i<lib.length;i++){
-    var first=lib[i];     
-    var second = [];
 
-    for (j=0;j<split.length;j++){
-      var querStr = split[j];
-       if (first.w.toUpperCase() == querStr.toUpperCase()){
-         second.push(first.r);
-         var third = second.join(" ");
-         console.log(third);
-       }
-        
-    }
-   
+    var quer = req.query.text;
+    var splitArray = quer.split(" ");
+    // console.log(split.join(" "));
+    var resString = "";
 
-  }
-  res.json({
-    "success": {
-        "total": 1
-    },
-    "contents": {
-        "translated": third ,
-        "text": req.query.text || "text missing",
-        "translation": "minion"
+
+    for (j = 0; j < splitArray.length; j++) {
+        var singleString = splitArray[j];
+
+
+        for (i = 0; i < lib.length; i++) {
+            var libString = lib[i];
+            var resArray = [];
+
+            if (libString.w.toUpperCase() == singleString.toUpperCase()) {
+
+                resArray.push(libString.r);
+                resString = `${resString} ${libString.r}`;
+            } else {
+                // resArray.push(singleString);
+                // resString = `${resString} ${singleString}`;
+
+            }
+        }
+        console.log(resString);
     }
-})
+    res.json({
+        "success": {
+            "total": 1
+        },
+        "contents": {
+            "translated": resString,
+            "text": req.query.text || "text missing",
+            "translation": "minion"
+        }
+    })
 });
 
 app.listen(3000, () => {
-  console.log('server started');
+    console.log('server started');
 });
