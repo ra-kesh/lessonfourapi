8839e85bc25c9f0b8803769bee19254055e7d437
Rakesh <rakesh1q2w@gmail.com>
2020-12-08 11:49:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const lib = require("./lib");
+
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+
+app.get("/", (req, res) => {
+  res.send(" Go to translate")
+})
+
+
+
+app.get('/translate/minion.json', (req, res) => {
+  
+  var quer = req.query.text;
+  var split = quer.split(" ");
+  // console.log(split.join(" "));
+  
+  for (i=0;i<lib.length;i++){
+    var first=lib[i];     
+    var second = [];
+
+    for (j=0;j<split.length;j++){
+      var querStr = split[i];
+       if (first.w.toUpperCase() == querStr.toUpperCase()){
+         second.push(first.r);
+       }
+    }
+   
+
+  }
+  res.json({
+    "success": {
+        "total": 1
+    },
+    "contents": {
+        "translated": second.join(" ") ,
+        "text": req.query.text || "text missing",
+        "translation": "minion"
+    }
+})
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
